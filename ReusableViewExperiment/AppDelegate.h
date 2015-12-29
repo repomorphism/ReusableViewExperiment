32c181fbd29f5d2ea091926ed80bc924ff2dae7a
Hsing Liu <liu.hsing@gmail.com>
2015-12-29 23:17:15 +0800

    Initial Commit

---
+++ ReusableViewExperiment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReusableViewExperiment
+//
+//  Created by Paul on 12/29/15.
+//  Copyright © 2015 Mathemusician.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReusableViewExperiment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReusableViewExperiment
+//
+//  Created by Paul on 12/29/15.
+//  Copyright © 2015 Mathemusician.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
